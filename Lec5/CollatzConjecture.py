d50b6169ca581580714f5a771dadb72068ad70ac
tkumarg <tkumarg@vmware.com>
2019-07-19 15:31:19 +0530

    Added Lec5

---
+++ Lec5/CollatzConjecture.py
@@ -0,0 +1,15 @@
+# Collatz conjecture
+
+n = int(input('Enter a number: '))
+print(n)
+count = 0
+while n != 1:
+    count += 1
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = 3*n + 1
+    print(n)
+
+print("We have reached 1 in ", count, " steps.")
+
---
+++ Lec5/LoopsAndIterations.py
@@ -0,0 +1,16 @@
+i = 0
+
+while i < 10:
+    print(i)
+    i += 1
+
+# This can also be written in FOR loop as
+
+for i in range(10):
+    print(i)
+
+
+# This can also be written with starting value and iteration
+
+for i in range(1, 10, 2):
+    print(i)
