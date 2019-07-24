6ebb7666cbd4d27b1d2b50e8cb473b23deb5c0ec
trishul kumar <g.trishul@gmail.com>
2019-07-24 15:53:34 +0530

    Updated ExtractEvents code

--- Events/ExtractEvents.py
+++ Events/ExtractEvents.py
@@ -1,3 +1,4 @@
 import pandas as pd
-datafile = pd.read_excel("data/ASK1563581.xlsx")
-datafile.head
+datafile = pd.read_excel(r'data/ASK1563581.xlsx', sheet_name='Sheet1')
+
+print(datafile.head())
