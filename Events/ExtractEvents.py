9bac9942c4ec08f2a29248d2ff540551be50ade5
tkumarg <tkumarg@vmware.com>
2019-07-25 15:50:57 +0530

    Updated ExtractEvents and added a temp data file

--- Events/ExtractEvents.py
+++ Events/ExtractEvents.py
@@ -1,4 +1,19 @@
 import pandas as pd
 datafile = pd.read_excel(r'data/ASK1563581.xlsx', sheet_name='Sheet1')
 
-print(datafile.head())
+#print(datafile.head())
+
+
+#print(datafile.groupby('DBName').count())
+#print(datafile.duplicated('EventName').count())
+
+grouped = datafile['EventName'].groupby(datafile['DBName'])
+listoflists = []
+for name, group in grouped:
+    name = []
+    for i in group:
+        name.append(i)
+    listoflists.append(name)
+#print(listoflists)
+print(len(set.intersection(*[set(list) for list in listoflists])))
+print(set.intersection(*[set(list) for list in listoflists]))
